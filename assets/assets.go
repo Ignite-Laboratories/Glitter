01d5fe4bf41e581c963642a0b3884cce8814737b
Robert Petz <rpetz@ignitelabs.net>
2025-02-21 14:34:36 -0800

    Made this a little more idiomatic
    go:embed has some quirks, but I don't mind them

--- assets/assets.go
+++ assets/assets.go
@@ -8,21 +8,18 @@ import (
 )
 
 // All provides the file systems for each folder within the "assets" directory.  For easier access, please use assets.Get.
-var All = []embed.FS{Shaders, Audio}
-
-// Audio provides an embed.FS of the "audio" directory.  For easier access, please use assets.Get.
 //
-//go:embed audio/*
-var Audio embed.FS
+//go:embed content/*
+var All embed.FS
 
 // AudioFiles returns all files in the "audio" directory.
 func (g get) AudioFiles() []string {
-	return g.Contents("audio")
+	return g.Files("content/audio")
 }
 
 // AudioFile returns back a specific audio file rooted in the "audio" directory.
 func (g get) AudioFile(path string) []byte {
-	data, err := Audio.ReadFile("audio/" + path)
+	data, err := All.ReadFile("content/audio/" + path)
 	if err != nil {
 		err = errs.Wrap(err, fmt.Errorf("failed to read audio file: %s", path))
 		panic(errs.Detailed(err))
@@ -30,19 +27,14 @@ func (g get) AudioFile(path string) []byte {
 	return data
 }
 
-// Shaders provides an embed.FS of the "shaders" directory.  For easier access, please use assets.Get.
-//
-//go:embed shaders/*
-var Shaders embed.FS
-
 // Shaders returns all files in the "shaders" directory.
 func (g get) Shaders() []string {
-	return g.Contents("shaders")
+	return g.Files("content/shaders")
 }
 
 // Shader returns back a specific shader file rooted in the "shaders" directory.
 func (g get) Shader(path string) []byte {
-	data, err := Shaders.ReadFile("shaders/" + path)
+	data, err := All.ReadFile("content/shaders/" + path)
 	if err != nil {
 		err = errs.Wrap(err, fmt.Errorf("failed to read shader: %s", path))
 		panic(errs.Detailed(err))
@@ -55,17 +47,18 @@ type get int
 // Get provides a fluent API into the local Glitter assets.
 var Get get
 
-// Contents returns back the files contained in a particular subfolder by passing root along to fs.WalkDir
-// and filtering out only the files found.
-func (g get) Contents(root string) []string {
-	var output []string
-	for _, fileSystem := range All {
-		fs.WalkDir(fileSystem, root, func(path string, d fs.DirEntry, err error) error {
-			if err == nil && !d.IsDir() {
-				output = append(output, path)
-			}
-			return nil
-		})
+// Files returns back the files contained in a particular subfolder by passing root along to fs.WalkDir
+// and filtering out only the files found.  If no root is provided, it defaults to "."
+func (g get) Files(root ...string) []string {
+	if len(root) == 0 {
+		root = append(root, ".")
 	}
+	var output []string
+	fs.WalkDir(All, root[0], func(path string, d fs.DirEntry, err error) error {
+		if err == nil && !d.IsDir() {
+			output = append(output, path)
+		}
+		return nil
+	})
 	return output
 }
